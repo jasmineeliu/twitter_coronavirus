6e36550ec9e8f39e06320e95abe8f2a17426755c
Jasmine Liu <jasmineliu331@gmail.com>
2026-02-27 10:48:51 -0800

    finishing plots

--- src/visualize.py
+++ src/visualize.py
@@ -24,22 +24,37 @@ if args.percent:
         counts[args.key][k] /= counts['_all'][k]
 
 # getting the count values
-items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)
+items = sorted(counts[args.key].items(), key=lambda item: (item[1],item[0]), reverse=True)[:10]
 top_10 = []
 top_10_values = []
-i = 0
-for k,v in items:
+top_items = sorted(items, key=lambda item: item[1])
+for k,v in top_items:
     top_10.append(k)
     top_10_values.append(v)
-    i += 1
-    if i == 10:
-        break
+print(top_10)
+print(top_10_values)
 
 #plotting
 import matplotlib.pyplot as plt
-fig, ax = plt.subplots()
+import matplotlib
+import matplotlib.font_manager as fm
+matplotlib.rcParams['font.family'] = 'UnDotum'
+matplotlib.rcParams['axes.unicode_minus'] = False
+if 'lang' in args.input_path:
+    file_type = "Language"
+else:
+    file_type = "Country"
 
-ax.bar(top_10, top_10_values, width=1, edgecolor="white", linewidth=0.7)
+import matplotlib.pyplot as plt
+import matplotlib
+import matplotlib.font_manager as fm
+
+for font in fm.findSystemFonts():
+    print(font)
+plt.bar(top_10, top_10_values, width=1, edgecolor="white", linewidth=0.7)
+plt.title("Tweets with " + args.key + " by " + file_type)
+plt.ylabel("Number of tweets")
+plt.xlabel(file_type)
 plt.savefig(args.output_png)
 
 plt.show()
